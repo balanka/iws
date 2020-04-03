67fca4fd10284529110fc00e14792e46a106432b
B.Mady <kaba.bintou-mady_fr@guj.de>
2020-04-03 12:24:20 +0200

    added code for the new  bankstatement module

---
+++ iws/user-api/src/main/scala/com/kabasoft/iws/endpoint/BankStatementEndpoints.scala
@@ -0,0 +1,96 @@
+package com.kabasoft.iws.endpoint
+
+import java.util.Date
+import cats.data.Validated.{Invalid, Valid}
+import cats.effect.Effect
+import cats.implicits._
+import com.kabasoft.iws.error.json.ErrorsJson
+import com.kabasoft.iws.pagination.PaginationValidator
+import com.kabasoft.iws.pagination.Pagination.PageSizeMatcher
+import com.kabasoft.iws.pagination.Pagination._
+import com.kabasoft.iws.service.BankStatementService
+import com.kabasoft.iws.domain.BankStatement
+import io.circe.{Decoder, Encoder}
+import io.circe.{Decoder, Encoder}
+import io.circe.generic.auto._
+import io.circe.syntax._
+import org.http4s.HttpRoutes
+import org.http4s.circe._
+import cats.implicits._
+import io.circe.syntax._
+import org.http4s._
+import org.http4s.circe._
+import org.http4s.dsl.Http4sDsl
+
+class BankStatementEndpoints[F[_]: Effect] extends Http4sDsl[F] {
+
+  def routes(service: BankStatementService[F]): HttpRoutes[F] = get(service) <+> list(service)
+
+  private def list(service: BankStatementService[F]): HttpRoutes[F] =
+    HttpRoutes.of[F] {
+      case request @ POST -> Root / "bs" =>
+        for {
+          masterfile <- request.decodeJson[BankStatement]
+          created <- service.create(masterfile)
+          resp <- Created(created.asJson)
+        } yield resp
+
+      case DELETE -> Root / "bs" / id =>
+        service.delete(id) *>
+          Ok()
+      case request @ PATCH -> Root / "bs" =>
+        for {
+          masterfile <- request.decodeJson[BankStatement]
+          updated <- service.update(masterfile)
+          response <- Ok(updated.asJson) //.putHeaders(Header("X-Auth-Token", "value"))
+        } yield response
+
+      case GET -> Root / "bs" :? PageMatcher(maybePage) :? PageSizeMatcher(maybePageSize) =>
+        val page = maybePage.getOrElse(DefaultPage)
+        val pageSize = maybePageSize.getOrElse(DefaultPageSize)
+
+        PaginationValidator.validate(page, pageSize) match {
+          case Valid(pagination) =>
+            val (from, until) = pagination.range
+            for {
+              retrieved <- service.list(from, until + 1)
+              hasNext = retrieved.size > until
+              masterfile = if (hasNext) retrieved.init else retrieved
+              //response <- Ok(masterfile.asJson)
+              response <- Ok("{ \"hits\": " + masterfile.asJson + " }") //, `Access-Control-Allow-Origin`("*"))
+
+            } yield response
+          case Invalid(errors) =>
+            BadRequest(ErrorsJson.from(errors).asJson)
+        }
+    }
+
+  private def get(service: BankStatementService[F]): HttpRoutes[F] =
+    HttpRoutes.of[F] {
+      case GET -> Root / "bs" / id =>
+        service.getBy(id).flatMap {
+          case Some(found) => Ok(found.asJson)
+          case None => NotFound("")
+        }
+      case GET -> Root / "bsmd" / IntVar(modelid) :? PageMatcher(maybePage) :? PageSizeMatcher(maybePageSize) =>
+        val page = maybePage.getOrElse(DefaultPage)
+        val pageSize = maybePageSize.getOrElse(DefaultPageSize)
+        PaginationValidator.validate(page, pageSize) match {
+          case Valid(pagination) =>
+            val (from, until) = pagination.range
+            for {
+              retrieved <- service.getByModelId(modelid, from, until)
+              hasNext = retrieved.size > until
+              transaction = if (hasNext) retrieved.init else retrieved
+              response <- Ok("{ \"hits\": " + transaction.asJson + " }") //, `Access-Control-Allow-Origin`("*"))
+
+            } yield response
+          case Invalid(errors) =>
+            BadRequest(ErrorsJson.from(errors).asJson)
+        }
+    }
+}
+object BankStatementEndpoints {
+  def apply[F[_]: Effect](service: BankStatementService[F]): HttpRoutes[F] =
+    new BankStatementEndpoints[F].routes(service)
+}
