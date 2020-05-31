94111e79f05aa8778e176e95a5d76d16ef865789
B.Mady <kaba.bintou-mady_fr@guj.de>
2020-05-31 10:30:41 +0200

    commented out

--- iws/domain/src/test/scala/com/toracoya/petstore/pet/package.scala
+++ iws/domain/src/test/scala/com/toracoya/petstore/pet/package.scala
@@ -1,8 +1,10 @@
 package com.toracoya.petstore
 
 package object pet {
-
+/*
   val pet1: Pet = Pet(MasterfileId("1"), "pet1")
   val pet2: Pet = Pet(MasterfileId("2"), "pet2")
   val pet3: Pet = Pet(MasterfileId("3"), "pet3")
+
+ */
 }
