808ccd47b9a44d06131785c50c1686e3defa014d
B.Mady <kaba.bintou-mady_fr@guj.de>
2020-06-24 17:17:11 +0200

    added authentication added to the repo

---
+++ iws/user-api/src/main/scala/com/kabasoft/iws/endpoint/package.scala
@@ -0,0 +1,10 @@
+package com.kabasoft.iws
+import com.kabasoft.iws.repository.doobie.User
+import org.http4s.Response
+import tsec.authentication.{AugmentedJWT, SecuredRequest, TSecAuthService}
+
+package object endpoint {
+  type AuthService[F[_], Auth] = TSecAuthService[User, AugmentedJWT[Auth, Long], F]
+  type AuthEndpoint[F[_], Auth] =
+    PartialFunction[SecuredRequest[F, User, AugmentedJWT[Auth, Long]], F[Response[F]]]
+}
