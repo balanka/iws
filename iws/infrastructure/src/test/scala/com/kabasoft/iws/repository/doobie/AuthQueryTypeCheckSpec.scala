786b30eaafc727bdb4eff7acd2d3a46af4b757ee
B.Mady <kaba.bintou-mady_fr@guj.de>
2020-07-02 13:48:34 +0200

    removed unused files

---
+++ iws/infrastructure/src/test/scala/com/kabasoft/iws/repository/doobie/AuthQueryTypeCheckSpec.scala
@@ -0,0 +1,32 @@
+package com.kabasoft.iws.repository.doobie
+
+import cats.effect.IO
+import doobie.scalatest.IOChecker
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import IwsRepoArbitraries._
+import tsec.mac.jca.HMACSHA256
+import tsec.authentication.AugmentedJWT
+import tsec.common.SecureRandomId
+import org.scalatest.matchers.should.Matchers
+
+class AuthQueryTypeCheckSpec extends AnyFunSuite with Matchers with ScalaCheckPropertyChecks with IOChecker {
+  override def transactor: doobie.Transactor[IO] = testTransactor
+
+  import AuthSQL._
+
+  test("Typecheck auth queries") {
+    forAll { jwt: AugmentedJWT[HMACSHA256, Long] =>
+      check(insert(jwt))
+    }
+    forAll { jwt: AugmentedJWT[HMACSHA256, Long] =>
+      check(update(jwt))
+    }
+    forAll { id: SecureRandomId =>
+      check(select(id))
+    }
+    forAll { id: SecureRandomId =>
+      check(delete(id))
+    }
+  }
+}
---
+++ iws/infrastructure/src/test/scala/com/kabasoft/iws/repository/doobie/UserQueryTypeCheckSpec.scala
@@ -0,0 +1,26 @@
+package com.kabasoft.iws.repository.doobie
+
+import org.scalatest.funsuite.AnyFunSuite
+import cats.effect.IO
+import doobie.scalatest.IOChecker
+import doobie.util.transactor.Transactor
+
+import IwsRepoArbitraries.user
+import org.scalatest.matchers.should.Matchers
+
+class UserQueryTypeCheckSpec extends AnyFunSuite with Matchers with IOChecker {
+  override val transactor: Transactor[IO] = testTransactor
+
+  import UserSQL._
+
+  test("Typecheck user queries") {
+    user.arbitrary.sample.map { u =>
+      check(insert(u))
+      check(byUserName(u.userName))
+      u.id.foreach(id => check(update(u, id)))
+    }
+    check(selectAll)
+    check(select(1L))
+    check(delete(1L))
+  }
+}
