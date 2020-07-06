59214c1bc67c6ceb6a6e8d977dc9204cf0477b43
B.Mady <kaba.bintou-mady_fr@guj.de>
2020-07-06 14:58:14 +0200

    added multi tenancy

--- iws/application/src/main/scala/com/kabasoft/iws/service/IWSService.scala
+++ iws/application/src/main/scala/com/kabasoft/iws/service/IWSService.scala
@@ -1,10 +1,10 @@
 package com.kabasoft.iws.service
 trait Service[F[_], A] {
   def create(item: A): F[Int]
-  def delete(item: String): F[Int]
-  def list(from: Int, until: Int): F[List[A]]
-  def getBy(id: String): F[Option[A]]
-  def getByModelId(modelid: Int, from: Int, until: Int): F[List[A]]
-  def findSome(from: Int, until: Int, model: String*): F[List[A]]
-  def update(model: A): F[List[Int]]
+  def delete(item: String, company: String): F[Int]
+  def list(from: Int, until: Int, company: String): F[List[A]]
+  def getBy(id: String, company: String): F[Option[A]]
+  def getByModelId(modelid: Int, from: Int, until: Int, company: String): F[List[A]]
+  def findSome(from: Int, until: Int, company: String, model: String*): F[List[A]]
+  def update(model: A, company: String): F[List[Int]]
 }
