54764472e595e0fe124bb9ec7370d153c49ac1f9
B.Mady <kaba.bintou-mady_fr@guj.de>
2021-01-10 11:03:22 +0100

    added company module

---
+++ iws/user-api/src/main/scala/com/kabasoft/iws/endpoint/CompanyEndpoints.scala
@@ -0,0 +1,102 @@
+package com.kabasoft.iws.endpoint
+
+import cats.data.Validated.{Invalid, Valid}
+import cats.effect.Sync
+import cats.implicits._
+import com.kabasoft.iws.auth.Auth
+import com.kabasoft.iws.domain.Company
+import com.kabasoft.iws.error.json.ErrorsJson
+import com.kabasoft.iws.pagination.Pagination._
+import com.kabasoft.iws.pagination.PaginationValidator
+import com.kabasoft.iws.repository.doobie.{CompanyService, User}
+import io.circe.generic.auto._
+import io.circe.syntax._
+import org.http4s.circe._
+import org.http4s.dsl.Http4sDsl
+import org.http4s.{EntityDecoder, HttpRoutes}
+import tsec.authentication.{AugmentedJWT, SecuredRequestHandler, asAuthed}
+import tsec.jwt.algorithms.JWTMacAlgo
+
+class CompanyEndpoints[F[_]: Sync, Auth: JWTMacAlgo] extends Http4sDsl[F] {
+
+  implicit val companyDecoder: EntityDecoder[F, Company] = jsonOf[F, Company]
+
+  private def list(service: CompanyService[F]): AuthEndpoint[F, Auth] = {
+    case req @ POST -> Root asAuthed _ =>
+      for {
+        instance <- req.request.as[Company]
+        created <- service.create(instance)
+        resp <- Created(created.asJson)
+      } yield resp
+    case DELETE -> Root / id asAuthed user =>
+      service.delete(id, user.company) *> Ok()
+
+    case req @ PATCH -> Root asAuthed user =>
+      for {
+        instance <- req.request.as[Company]
+        updated <- service.update(instance, user.company)
+        response <- Ok(updated.asJson)
+      } yield response
+
+    case GET -> Root :? OffsetMatcher(maybePage) :? PageSizeMatcher(maybePageSize) asAuthed user =>
+      val page = maybePage.getOrElse(DefaultPage)
+      val pageSize = maybePageSize.getOrElse(DefaultPageSize)
+
+      PaginationValidator.validate(page, pageSize) match {
+        case Valid(pagination) =>
+          val (from, until) = pagination.range
+          for {
+            retrieved <- service.list(from, until + 1, user.company)
+            hasNext = retrieved.size > until
+            vat = if (hasNext) retrieved.init else retrieved
+            response <- Ok("{ \"hits\": " + vat.asJson + " }")
+          } yield response
+        case Invalid(errors) =>
+          BadRequest(ErrorsJson.from(errors).asJson)
+      }
+  }
+
+  private def get(service: CompanyService[F]): AuthEndpoint[F, Auth] = {
+    case GET -> Root / id asAuthed user =>
+      service.getBy(id, user.company).flatMap {
+        case Some(found) => Ok(found.asJson)
+        case None => NotFound("")
+      }
+    case GET -> Root / "companymd" / IntVar(modelid) :? OffsetMatcher(maybePage)
+          :? PageSizeMatcher(maybePageSize) asAuthed user =>
+      val page = maybePage.getOrElse(DefaultPage)
+      val pageSize = maybePageSize.getOrElse(DefaultPageSize)
+      PaginationValidator.validate(page, pageSize) match {
+        case Valid(pagination) =>
+          val (from, until) = pagination.range
+          for {
+            retrieved <- service.getByModelId(modelid, from, until, user.company)
+            hasNext = retrieved.size > until
+            vat = if (hasNext) retrieved.init else retrieved
+            response <- Ok("{ \"hits\": " + vat.asJson + " }")
+
+          } yield response
+        case Invalid(errors) =>
+          BadRequest(ErrorsJson.from(errors).asJson)
+      }
+  }
+
+  def endpoints(
+    service: CompanyService[F],
+    auth: SecuredRequestHandler[F, Long, User, AugmentedJWT[Auth, Long]]
+  ): HttpRoutes[F] = {
+    val authEndpoints: AuthService[F, Auth] = {
+      Auth.allRolesHandler(list(service).orElse(get(service))) {
+        Auth.adminOnly(list(service).orElse(get(service)))
+      }
+    }
+
+    auth.liftService(authEndpoints)
+  }
+}
+object CompanyEndpoints {
+  def endpoints[F[_]: Sync, Auth: JWTMacAlgo](
+    service: CompanyService[F],
+    auth: SecuredRequestHandler[F, Long, User, AugmentedJWT[Auth, Long]]
+  ): HttpRoutes[F] = new CompanyEndpoints[F, Auth].endpoints(service, auth)
+}
